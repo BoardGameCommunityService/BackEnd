a865fe55235b7bf93552276099460676d49c5bc4
devsungyeon <sungyeonlee1103@gmail.com>
2025-11-14 19:52:37 +0900

    Initial commit

---
+++ src/main/java/com/boardmate/BoardmateApplication.java
@@ -0,0 +1,13 @@
+package com.boardmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boardmate/BoardmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
