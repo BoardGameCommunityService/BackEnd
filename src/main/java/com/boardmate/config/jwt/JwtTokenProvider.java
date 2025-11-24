56329e0b38eb1c816b208b5880d720bbf8fd4f2c
devsungyeon <sungyeonlee1103@gmail.com>
2025-11-24 23:36:04 +0900

    fix: Use SecretKey instead of Key for JJWT compatibility

--- src/main/java/com/boardmate/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/boardmate/config/jwt/JwtTokenProvider.java
@@ -5,13 +5,13 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
 
-    private final Key key;
+    private final SecretKey key;
     private final long accessTokenValidityMs;
     private final long refreshTokenValidityMs;
 
